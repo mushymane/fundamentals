0d47ef1bafd726921fab0ba4d9e0936ac8ae83ef
Luigi Lantin <mushymanee@gmail.com>
2021-03-09 21:26:23 -0800

    linked list implementation

---
+++ Data Structures and Algorithms/javascript/linkedlist.js
@@ -0,0 +1,54 @@
+// 57 --> 25 --> 21
+
+// let myLinkedList = {
+//     head: {
+//         value: 10,
+//         next: {
+//             value: 5,
+//             next: {
+//                 value: 16,
+//                 next: null
+//             }
+//         }
+//     }
+// }
+
+
+class LinkedList {
+    constructor(value) {
+        this.head = {
+            value: value,
+            next: null
+        };
+        this.tail = this.head;
+        this.length = 1;
+    }
+
+    append(value) {
+        const newNode = {
+            value: value,
+            next: null
+        };
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
+        return this;
+    }
+
+    prepend(value) {
+        const newNode = {
+            value: value,
+            next: null
+        };
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+        return this;
+    }
+}
+
+const myLinkedList = new LinkedList(57);
+console.log(myLinkedList.append(25));
+console.log(myLinkedList.append(21));
+console.log(myLinkedList.prepend(4));
+//console.log(myLinkedList);
