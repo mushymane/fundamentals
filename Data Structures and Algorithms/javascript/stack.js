61d7f4968c5aade215d236d4db1529630ee6798e
Luigi Lantin <mushymanee@gmail.com>
2021-03-11 23:05:17 -0800

    stack and queue implementation

---
+++ Data Structures and Algorithms/javascript/stack.js
@@ -0,0 +1,80 @@
+// linked list
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
+
+    peek() {
+        return this.top;
+    }
+
+    push(value) {
+        const newNode = new Node(value);
+        if (this.length === 0) {
+            this.top = newNode;
+            this.bottom = newNode;
+        } else {
+            const holdingPointer = this.top;
+            this.top = newNode;
+            this.top.next = holdingPointer;
+        }
+        this.length++;
+        return this;
+    }
+
+    pop() {
+        if (!this.top) {
+            return null;
+        }
+        if (this.top === this.bottom) {
+            this.bottom = null;
+        }
+        const holdingPointer = this.top;
+        this.top = this.top.next;
+        this.length--;
+        return this;
+    }
+    //isEmpty
+}
+
+// array
+class ArrayStack {
+    constructor() {
+        this.array = [];
+    }
+
+    peek() {
+        return this.array[this.array.length - 1];
+    }
+
+    push(value) {
+        this.array.push(value);
+        return this;
+    }
+
+    pop() {
+        this.array.pop();
+        return this;
+    }
+    //isEmpty
+}
+
+const myStack = new Stack();
+// const myStack = new ArrayStack();
+
+console.log(myStack.push('bitcoin'))
+console.log(myStack.push('ether'))
+console.log(myStack.push('cardano'))
+console.log(myStack.pop())
+console.log(myStack.pop())
+// console.log(myStack.push('cosmos'))
