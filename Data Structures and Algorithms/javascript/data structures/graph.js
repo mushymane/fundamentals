507635ac8c58125f4d82b678c5502375d837e0b5
Luigi Lantin <mushymanee@gmail.com>
2021-03-13 19:27:33 -0800

    graph

---
+++ Data Structures and Algorithms/javascript/data structures/graph.js
@@ -0,0 +1,78 @@
+// undirected, unweighted graph using adjacency list
+
+class Graph {
+    constructor() {
+        this.numberOfNodes = 0;
+        this.adjacentList = {};
+    }
+
+    addVertex(node) {
+        this.adjacentList[node] = [];
+        this.numberOfNodes++;
+    }
+
+    addEdge(node1, node2) {
+        //undirected graph
+        this.adjacentList[node1].push(node2);
+        this.adjacentList[node2].push(node1);
+    }
+
+    showConnections() {
+        const allNodes = Object.keys(this.adjacentList);
+        for (let node of allNodes) {
+            let nodeConnections = this.adjacentList[node];
+            let connections = "";
+            let vertex;
+            for (vertex of nodeConnections) {
+                connections += vertex + " ";
+            }
+            console.log(node + "-->" + connections);
+        }
+    }
+}
+
+const myGraph = new Graph();
+myGraph.addVertex('0');
+myGraph.addVertex('1');
+myGraph.addVertex('2');
+myGraph.addVertex('3');
+myGraph.addVertex('4');
+myGraph.addVertex('5');
+myGraph.addVertex('6');
+myGraph.addEdge('3', '1');
+myGraph.addEdge('3', '4');
+myGraph.addEdge('4', '2');
+myGraph.addEdge('4', '5');
+myGraph.addEdge('1', '2');
+myGraph.addEdge('1', '0');
+myGraph.addEdge('0', '2');
+myGraph.addEdge('6', '5');
+myGraph.showConnections();
+
+
+
+
+
+/*
+// Edge list
+const graph = [[0, 2], [2, 3], [2, 1], [1, 3]];
+
+// Adjacent list
+const graph = [[2], [2, 3], [0, 1, 3], [1, 2]]; //the index of array is the value of actual graph node
+// i.e. index 1 connect to 2, index 2 is connected to 2 and 3, index 3 connected to 0, 1, and 3
+
+// Adjacent matrix
+const graph = [
+    [0, 0, 1, 0],
+    [0, 0, 1, 1],
+    [1, 1, 0, 1],
+    [0, 1, 1, 0]
+]; // node 0 is connected to 2, node 1 is connected to 2 and 3, node 2 is connected to 0, 1, and 3
+
+const graph = {
+    0: [0, 0, 1, 0],
+    1: [0, 0, 1, 1],
+    2: [1, 1, 0, 1],
+    3: [0, 1, 1, 0]
+};
+*/
