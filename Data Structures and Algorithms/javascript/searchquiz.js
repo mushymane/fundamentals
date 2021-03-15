b3a6b930fdccb8e8841e7ed911b24127cfdd45b0
Luigi Lantin <mushymanee@gmail.com>
2021-03-14 19:33:35 -0700

    bfs

---
+++ Data Structures and Algorithms/javascript/searchquiz.js
@@ -0,0 +1,12 @@
+//If you know a solution is not far from the root of the tree:
+    // bfs
+//If the tree is very deep and solutions are rare: 
+    // bfs (dfs will take long)
+//If the tree is very wide:
+    // dfs (bfs will need too much memory)
+//If solutions are frequent but located deep in the tree:
+    // dfs
+//Determining whether a path exists between two nodes:
+    // dfs
+//Finding the shortest path:
+    // bfs
